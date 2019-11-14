609a254b35f6ceb46292d679d21739e2b219ed26
JoaoReis27 <JoaoReis27@users.noreply.github.com>
2019-11-14 15:42:11 +0000

    WebApp

---
+++ WebApp Bonecos/src/com/bonecos/kusco/jdbc/ResourceManager.java
@@ -0,0 +1,53 @@
+package com.bonecos.kusco.jdbc;
+
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+
+public class ResourceManager
+{
+	public static synchronized Connection getConnection()
+	throws NamingException, SQLException
+	{
+		InitialContext initCtx = new InitialContext();
+		Object obj = initCtx.lookup( "java:comp/env/JDBC/F16_Bonecos" );
+		DataSource ds = (DataSource) obj;
+		return ds.getConnection();
+	}
+
+
+	public static void close(Connection conn)
+	{
+		try {
+			if (conn != null) conn.close();
+		}
+		catch (SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+	}
+
+	public static void close(PreparedStatement stmt)
+	{
+		try {
+			if (stmt != null) stmt.close();
+		}
+		catch (SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rs)
+	{
+		try {
+			if (rs != null) rs.close();
+		}
+		catch (SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+
+	}
+
+}
