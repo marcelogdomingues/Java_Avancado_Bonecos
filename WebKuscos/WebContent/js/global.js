935e178e1a9ed644a50fc72c9488bf8462aa8f7a
marcelogdomingues <marcelo.dominugues.ul@gmail.com>
2019-11-15 15:24:17 +0000

    Front - End

---
+++ WebKuscos/WebContent/js/global.js
@@ -0,0 +1,104 @@
+(function ($) {
+    'use strict';
+    try {
+        $('.js-datepicker').daterangepicker({
+            "singleDatePicker": true,
+            "showDropdowns": true,
+            "autoUpdateInput": false,
+            locale: {
+                format: 'DD/MM/YYYY'
+            },
+        });
+    
+        var myCalendar = $('.js-datepicker');
+        var isClick = 0;
+    
+        $(window).on('click',function(){
+            isClick = 0;
+        });
+    
+        $(myCalendar).on('apply.daterangepicker',function(ev, picker){
+            isClick = 0;
+            $(this).val(picker.startDate.format('DD/MM/YYYY'));
+    
+        });
+    
+        $('.js-btn-calendar').on('click',function(e){
+            e.stopPropagation();
+    
+            if(isClick === 1) isClick = 0;
+            else if(isClick === 0) isClick = 1;
+    
+            if (isClick === 1) {
+                myCalendar.focus();
+            }
+        });
+    
+        $(myCalendar).on('click',function(e){
+            e.stopPropagation();
+            isClick = 1;
+        });
+    
+        $('.daterangepicker').on('click',function(e){
+            e.stopPropagation();
+        });
+    
+    
+    } catch(er) {console.log(er);}
+    /*[ Select 2 Config ]
+        ===========================================================*/
+    
+    try {
+        var selectSimple = $('.js-select-simple');
+    
+        selectSimple.each(function () {
+            var that = $(this);
+            var selectBox = that.find('select');
+            var selectDropdown = that.find('.select-dropdown');
+            selectBox.select2({
+                dropdownParent: selectDropdown
+            });
+        });
+    
+    } catch (err) {
+        console.log(err);
+    }
+
+
+    
+
+})(jQuery);
+
+//Nome - Aceita apenas Letras
+
+$(function () {
+    $('.txtFirstName').keydown(function (e) {
+        if (e.shiftKey || e.ctrlKey || e.altKey) {
+            e.preventDefault();
+        } else {
+            var key = e.keyCode;
+            if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+                e.preventDefault();
+                $("#errmsg2").html("Apenas é permitido Letras!").show().fadeOut("slow");
+            return false;
+            }
+        }
+    });
+});
+
+
+
+//Cartão de Cidadão - Aceita apenas Números
+
+
+    $(".allownumericwithoutdecimal").on("keypress keyup blur",function (event) {    
+        $(this).val($(this).val().replace(/[^\d].+/, ""));
+         if ((event.which < 48 || event.which > 57)) { /* FUNCIONAMENTO JQUERY: 
+            Para eventos de teclado ou rato, essa propriedade indica a tecla ou botão específico que foi pressionado. */ 
+            $("#errmsg").html("Apenas é permitido Números!").show().fadeOut("slow");
+            return false;
+            
+         }
+     });
+
+     
