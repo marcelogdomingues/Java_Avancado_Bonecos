9477bf3879a0d8261368ce050fd9a8bc3931f361
inesenmartins <inesenmartins@users.noreply.github.com>
2019-11-15 10:06:26 +0000

    WebKuscos

---
+++ WebKuscos/src/com/bonecos/kuscos/jdbc/AbstractDAO.java
@@ -0,0 +1,14 @@
+package com.bonecos.kuscos.jdbc;
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ * Base class for Oracle 9i DAO classes.
+ *
+ * This is a customizable template within FireStorm/DAO.
+ */
+public class AbstractDAO
+{
+	
+}
