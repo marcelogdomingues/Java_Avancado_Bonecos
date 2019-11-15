b6335b86c50761cb9a1ff2d92f0105fa30c06d5b
marcelogdomingues <marcelo.dominugues.ul@gmail.com>
2019-11-15 17:10:48 +0000

    Calendiario Fix.

--- WebKuscos/WebContent/js/global.js
+++ WebKuscos/WebContent/js/global.js
@@ -33,6 +33,42 @@
                 myCalendar.focus();
             }
         });
+
+        /*Resgito*/
+
+        $('.js-datepicker2').daterangepicker({
+            "singleDatePicker": true,
+            "showDropdowns": true,
+            "autoUpdateInput": false,
+            locale: {
+                format: 'DD/MM/YYYY'
+            },
+        });
+    
+        var myCalendar = $('.js-datepicker2');
+        var isClick = 0;
+    
+        $(window).on('click',function(){
+            isClick = 0;
+        });
+    
+        $(myCalendar).on('apply.daterangepicker',function(ev, picker){
+            isClick = 0;
+            $(this).val(picker.startDate.format('DD/MM/YYYY'));
+    
+        });
+    
+        $('.js-btn-calendar2').on('click',function(e){
+            e.stopPropagation();
+    
+            if(isClick === 1) isClick = 0;
+            else if(isClick === 0) isClick = 1;
+    
+            if (isClick === 1) {
+                myCalendar.focus();
+            }
+        });
+        
     
         $(myCalendar).on('click',function(e){
             e.stopPropagation();
