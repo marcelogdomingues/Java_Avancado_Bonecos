6c942e339e04b5da33fd4602c0fbabe4ebae84bf
marcelogdomingues <marcelo.dominugues.ul@gmail.com>
2019-11-15 16:59:54 +0000

    Sem erros

--- WebKuscos/WebContent/js/global.js
+++ WebKuscos/WebContent/js/global.js
@@ -79,7 +79,7 @@ $(function () {
             var key = e.keyCode;
             if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
                 e.preventDefault();
-                $("#errmsg2").html("Apenas é permitido Letras!").show().fadeOut("slow");
+              //  $("#errmsg2").html("Apenas é permitido Letras!").show().fadeOut("slow");
             return false;
             }
         }
@@ -95,7 +95,7 @@ $(function () {
         $(this).val($(this).val().replace(/[^\d].+/, ""));
          if ((event.which < 48 || event.which > 57 )) { /* FUNCIONAMENTO JQUERY: 
             Para eventos de teclado ou rato, essa propriedade indica a tecla ou botão específico que foi pressionado. */ 
-            $("#errmsg").html("Apenas é permitido Números!").show().fadeOut("slow");
+          //  $("#errmsg").html("Apenas é permitido Números!").show().fadeOut("slow");
             return false;
             }
             
